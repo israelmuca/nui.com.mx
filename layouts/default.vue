315d258f09a7280025c34306a8ae0349f28b416d
Israel Muñoz <isra.munoz@gmail.com>
2019-04-14 22:38:06 -0500

    Stablished the base for the project, added some SCSS rules and did the Navbar

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main-container">
+    <Nav></Nav>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Nav from "~/components/Nav.vue"
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style>
+.main-container {
+  margin-top: 50px;
+  margin-right: 5%;
+  margin-left: 5%;
+}
+</style>
