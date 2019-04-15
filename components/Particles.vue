35cef49f2b0e1d0932941cad9874958147b786a3
Israel Muñoz <isra.munoz@gmail.com>
2019-04-15 12:48:04 -0500

    New bugs, new fixes

--- components/Particles.vue
+++ components/Particles.vue
@@ -1,33 +1,37 @@
 <template>
-    <vue-particles id="particles-js"
-      color="#fafafa"
-      :particleOpacity="0.5"
-      linesColor="#fafafa"
-      :particlesNumber="100"
-      shapeType="circle"
-      :particleSize="3"
-      :linesWidth="1"
-      :lineLinked="true"
-      :lineOpacity="0.2"
-      :linesDistance="200"
-      :moveSpeed="1"
-      :hoverEffect="true"
-      hoverMode="grab"
-      :clickEffect="true"
-      clickMode="push"
-    ></vue-particles>
+  <vue-particles
+    id="particles-js"
+    color="#fafafa"
+    :particleOpacity="0.5"
+    linesColor="#fafafa"
+    :particlesNumber="100"
+    shapeType="circle"
+    :particleSize="3"
+    :linesWidth="1"
+    :lineLinked="true"
+    :lineOpacity="0.2"
+    :linesDistance="200"
+    :moveSpeed="1"
+    :hoverEffect="true"
+    hoverMode="grab"
+    :clickEffect="true"
+    clickMode="push"
+  ></vue-particles>
 </template>
 
 <script>
-export default {
-    
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
 @import "~/assets/css/main.scss";
 
 #particles-js {
+  background-color: $dark-blue;
+  max-height: 200px;
+}
+
+canvas {
   background-color: $dark-blue;
   max-height: 200px;
 }
