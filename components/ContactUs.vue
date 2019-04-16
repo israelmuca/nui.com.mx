09a33d8aed45f9c3eb2d0c3cae2e4feeb2ffbf68
Israel Muñoz <isra.munoz@gmail.com>
2019-04-15 19:16:18 -0500

    Imported Forms from Bulma for the scss, modified the look of the ContactUs and updated my build process to handle the lambda

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -1,48 +1,70 @@
 <template>
   <div>
-    <h1 class="contact-title">Contáctanos</h1>
+    <h1 class="sections-title has-text-centered">Contáctanos</h1>
     <div v-if="!sent">
       <form name="contact" @submit.prevent="processForm" action="/.netlify/functions/sendGrid">
-        <div class="field">
-          <label class="label">Nombre</label>
-          <div class="control">
-            <input v-model="emaildata.name" class="input" type="text" name="name">
+        <div class="columns">
+          <div class="column">
+            <div class="field">
+              <label class="label">Nombre</label>
+              <div class="control">
+                <input v-model="emaildata.name" class="input" type="text" name="name">
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control">
+                <input v-model="emaildata.email" class="input" type="email" name="email">
+              </div>
+            </div>
           </div>
         </div>
-        <div class="field">
-          <label class="label">Email</label>
-          <div class="control">
-            <input v-model="emaildata.email" class="input" type="email" name="email">
+        <div class="columns">
+          <div class="column">
+            <div class="field">
+              <label class="label">Teléfono</label>
+              <div class="control">
+                <input v-model="emaildata.phone" class="input" type="tel" name="phone">
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="field">
+              <label class="label">Ciudad</label>
+              <div class="control">
+                <input v-model="emaildata.city" class="input" type="text" name="city">
+              </div>
+            </div>
           </div>
         </div>
-        <div class="field">
-          <label class="label">Teléfono</label>
-          <div class="control">
-            <input v-model="emaildata.phone" class="input" type="tel" name="phone">
+
+        <div class="columns">
+          <div class="column">
+            <div class="field">
+              <label class="label">Mensaje</label>
+              <div class="control">
+                <textarea
+                  v-model="emaildata.message"
+                  class="textarea"
+                  placeholder="Favor de escribir su mensaje aquí"
+                  name="message"
+                ></textarea>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="field">
-          <label class="label">Ciudad</label>
-          <div class="control">
-            <input v-model="emaildata.ciudad" class="input" type="text" name="city">
-          </div>
-        </div>
-        <div class="field">
-          <label class="label">Mensaje</label>
-          <div class="control">
-            <textarea
-              v-model="emaildata.message"
-              class="textarea"
-              placeholder="Favor de escribir su mensaje aquí"
-              name="message"
-            ></textarea>
-          </div>
-        </div>
-        <div class="field">
-          <div class="control">
-            <button type="submit" class="button send-contact">
-              <span class="button-text">Enviar</span>
-            </button>
+
+        <div class="columns is-centered">
+          <div class="column is-2 has-text-centered">
+            <div class="field">
+              <div class="control">
+                <button type="submit" class="button">
+                  <span class="button-text">Enviar</span>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </form>
@@ -87,4 +109,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~/assets/css/main.scss";
+
+.sections-title {
+  @include sections-title;
+  margin-bottom: 40px;
+}
+
+.label {
+  @include sections-content;
+}
+
+.button {
+  @include sections-content;
+  background-color: $dark-blue;
+  color: $white;
+  width: 150px;
+
+  &:hover {
+    background-color: $purple;
+    color: $white;
+  }
+}
 </style>
