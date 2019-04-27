35820c92f88b3cc7872f30b70323578ff097fba1
Israel Muñoz <isra.munoz@gmail.com>
2019-04-27 11:40:09 -0500

    Fixed a bug, importing a non-existing library that's not needed

--- plugins/maps.js
+++ plugins/maps.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import * as VueGoogleMaps from "~/node_modules/vue2-google-maps/src/main";
 
-require('dotenv').config()
-
 Vue.use(VueGoogleMaps, {
   load: {
     key: process.env.GMAPS_API_KEY
